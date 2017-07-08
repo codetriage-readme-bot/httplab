aabacc8a2b5739d6cc05c8fcaaa6d397232b570b
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-08 19:11:38 +0500

    travis test

--- spec/requests/api/v1/api_controller_spec.rb
+++ spec/requests/api/v1/api_controller_spec.rb
@@ -12,20 +12,10 @@ class Resource < ApplicationRecord
 end
 
 class ResourcePolicy < ApplicationPolicy
-  class Scope < ApplicationPolicy::Scope
-    def resolve
-      scope.all
-    end
-  end
 
-  def show?
-    true
+  def create?
+    user
   end
-
-  alias_method :index?, :show?
-  alias_method :create?, :show?
-  alias_method :update?, :show?
-  alias_method :destroy?, :show?
 
   def permitted_attributes
     [:content]
@@ -60,7 +50,7 @@ RSpec.describe API::V1::ResourcesController do
   let(:authenticated_header) {
     { 'Authorization' => "Bearer #{create(:user).api_token}" }
   }
-  let(:resource) { Resource.create!(content: 'something') }
+
   describe 'POST#create' do
     context 'when request is invalid returns status 422' do
       before { post '/api/v1/resources', headers: authenticated_header }
