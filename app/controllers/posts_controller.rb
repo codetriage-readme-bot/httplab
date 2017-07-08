a19438325d46df010c4c28803ee429bd5b713777
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-08 18:50:41 +0500

    pause

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,5 @@ class PostsController < ApplicationController
     users = User.all
     @top_posts_users = users.order('posts_count DESC').limit(5)
     @top_rating_users = users.order('vote_rating DESC').limit(5)
-    @top_average_rating_users = users.order('vote_rating DESC').limit(5)
   end
 end
