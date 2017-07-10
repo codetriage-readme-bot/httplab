a3305ee8583cd22a3bffa8e5df25a3d05762f688
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-10 15:04:50 +0500

    fixes some robocop issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,20 +9,20 @@ class User < ApplicationRecord
   after_initialize :init
 
   def init
-    self.vote_rating  ||= 0
+    self.vote_rating ||= 0
     self.posts_count ||= 0
   end
 
   # https://davidcel.is/posts/stop-validating-email-addresses-with-regex/
-  validates_format_of :email, with: /.*@.*/
+  validates_format_of :email, with: /\A.*@.*\z/
   validates_presence_of :password, if: :password_required?
   validate :password_match?, if: :password_required?
   alias_method :authenticate, :valid_password?
 
   def password_match?
-    self.errors[:password] << 'cannot be blank' if password.blank?
-    self.errors[:password_confirmation] << 'cannot be blank' if password_confirmation.blank?
-    self.errors[:password_confirmation] << 'does not match' if password != password_confirmation
+    errors[:password] << 'cannot be blank' if password.blank?
+    errors[:password_confirmation] << 'cannot be blank' if password_confirmation.blank?
+    errors[:password_confirmation] << 'does not match' if password != password_confirmation
     password.present? && password == password_confirmation
   end
 
