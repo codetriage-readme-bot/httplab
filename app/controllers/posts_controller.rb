4b4a525c1f8614c6132e309b35d5bc75f38373de
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-11 12:20:38 +0500

    magic number

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,16 @@
 class PostsController < ApplicationController
   def index
     top_users
-    @top_posts_message_rating = Post.all.order('message_rating DESC').limit(number)
+    @top_posts_message_rating = Post.all.order('message_rating DESC').limit(MAGIC_NUMBER)
   end
 
   private
 
   def top_users
     users = User.all
-    @top_users_posts = users.order('posts_count DESC').limit(number)
-    @top_users_average_rating = users.sort_by(&:avarage_rating).reverse.take(number)
+    @top_users_posts = users.order('posts_count DESC').limit(MAGIC_NUMBER)
+    @top_users_average_rating = users.sort_by(&:avarage_rating).reverse.take(MAGIC_NUMBER)
   end
 
-  def number
-    5
-  end
+  MAGIC_NUMBER = 5
 end
